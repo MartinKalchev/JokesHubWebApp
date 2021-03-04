65ee7712cf2549d1ecca2532393f5abe29084ec4
Martin Kalchev <martinkalchev97@gmail.com>
2021-03-04 09:39:03 +0200

    added project files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JokesHubWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
